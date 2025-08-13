86df47a8f12c68b19582ad77fe759bc4621ddcd3
Денис <forsidenis@yandex.ru>
2025-08-14 00:11:56 +0300

    ТЗ 6 спринта - выполнено

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -10,28 +10,43 @@ public interface TaskManager {
     List<Task> getAllTasks();
 
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     void createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
 
     // Методы для эпиков
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpicById(int id);
+
     void createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
 
     // Методы для подзадач
     List<Subtask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     Subtask getSubtaskById(int id);
+
     void createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
 
     // Дополнительные методы
     List<Subtask> getSubtasksByEpicId(int epicId);
+
     List<Task> getHistory();
 }
